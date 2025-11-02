43e6589f3da568da1ead378d043335304a7c9b77
Lukas7u1ehner <lukas.zulehner@outlook.com>
2025-11-02 21:52:20 +0100

    gutmman isins hinzugefügt

--- src/catalog.py
+++ src/catalog.py
@@ -64,8 +64,75 @@ KATALOG = {
     "ANDRITZ Aktie": "AT0000730007",
     "BAWAG Group Aktie": "AT0000BAWAG2",
     "Österreichische Post Aktie": "AT0000APOST4",
-    "Vienna Insurance Group Aktie": "AT00009VIG3"
+    "Vienna Insurance Group Aktie": "AT00009VIG3",
+
+    # --- Gutmann Investmentfonds (Neu aus dem Bild) ---
+    "Gutmann Core Equities": "AT0000720083",
+    "Gutmann Core Equities_2": "AT0000A1QCR4", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann CZK Bonds": "AT0000A2D7P2",
+    "Gutmann Eastern Europe Bonds": "AT0000A0PEX0",
+    "Gutmann Eastern Europe Bonds_2": "AT0000A0PEV8", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Eastern Europe Equities": "AT0000A0PEU6",
+    "Gutmann ESG Corporate Bonds": "AT0000A29TC4",
+    "Gutmann ESG Equities": "AT0000A15M75",
+    "Gutmann ESG Equities_2": "AT0000A269QG", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann ESG Equities_3": "AT0000A2KAD9", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann ESG Equities_4": "AT0000A1TQC4", # Die vierte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann ESG Euro Bonds": "AT0000A1S055",
+    "Gutmann ESG Euro Bonds_2": "AT0000A1S3G9", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann ESG Euro Short-Term Bonds": "AT0000A1S365",
+    "Gutmann ESG Global Bonds Strategy": "AT0000A2VXV4",
+    "Gutmann ESG Global Bonds Strategy_2": "AT0000A2VXZ1", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Euro Bond Opportunities": "AT0000A12PL3",
+    "Gutmann Euro Bonds": "AT0000856802",
+    "Gutmann Euro Corporate Bonds": "AT0000A1Q9W2",
+    "Gutmann Euro Open Duration Bonds": "AT0000A1W3M1",
+    "Gutmann Euro Short-Term Bonds": "AT0000611181",
+    "Gutmann Global Bonds Strategy": "AT0000A0X4Z2",
+    "Gutmann Global Bonds Strategy_2": "AT0000A2UWG5", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Dividends": "AT0000A0LXV5",
+    "Gutmann Global Dividends_2": "AT0000A0LXW1", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Dividends_3": "AT0000A1QCZ7", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Dividends_4": "AT0000A1Z9Q8", # Die vierte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Equities": "AT0000A3ZSC6",
+    "Gutmann Global Equities_2": "AT0000A3ZSE2", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Equities_3": "AT0000A1FHP0", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Equity Strategy": "AT0000A1EZS5",
+    "Gutmann Global Equity Strategy_2": "AT0000A1EZW7", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Global Inflation-Linked Bonds": "AT0000A1SL84",
+    "Gutmann International Bonds": "AT0000856810",
+    "Gutmann International Bonds_2": "AT0000A1QQX0", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Investor EUR 20": "AT0000779368",
+    "Gutmann Investor EUR 40": "AT0000779376",
+    "Gutmann Investor EUR 40_2": "AT0000A1JHQ8", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Investor EUR 40_3": "AT0000A1WZ41", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Investor Fundamental": "AT0000A1EZM2",
+    "Gutmann Investor USD 20": "AT0000A1EZN0",
+    "Gutmann Investor USD 40": "AT0000A1WZM3",
+    "Gutmann Investor USD 40_2": "AT0000A2T9F9", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Mündelsicherer Anleihefonds": "AT0000962287",
+    "Gutmann Mündelsicherer Anleihefonds_2": "AT0000A0E0Q5", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Pure Innovation": "AT0000A2VB68",
+    "Gutmann Pure Innovation_2": "AT0000A2VB96", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Reserve": "AT0000962295",
+    "Gutmann Reserve_2": "AT0000A0DCH2", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Reserve_3": "AT0000A1GUV9", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Reserve_4": "AT0000A264Z8", # Die vierte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann Strategie 365": "AT0000A1Z5Y5",
+    "Gutmann Tactical Alpha Strategy": "AT0000A2C2E8",
+    "Gutmann US Dividends": "AT0000A0BQU2",
+    "Gutmann US Dividends_2": "AT0000A1NSM4", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann US Portfolio": "AT0000712694",
+    "Gutmann US-Portfolio": "AT0000712629",
+    "Gutmann USD Bonds Strategy": "AT0000A1Z9L1",
+    "Gutmann USD Bonds Strategy_2": "AT0000A2N2N1", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann USD Bonds Strategy_3": "AT0000A1QE26", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann USD Mid-Term Bonds": "AT0000A33R10",
+    "Gutmann USD Mid-Term Bonds_2": "AT0000A1HSX1", # Die zweite Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann USD Mid-Term Bonds_3": "AT0000A33RS2", # Die dritte Zeile mit gleichem Namen, aber anderer ISIN
+    "Gutmann USD Reserve": "AT0000A0BRPX",
+    "Gutmann USD Short-Term Bonds": "AT0000A0BR28",
+    "Gutmann Vorsorgefonds": "AT0000856828"
     
     # (Du kannst hier beliebig viele hinzufügen)
 }
-
